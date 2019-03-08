9ba352b90b8d8d2440beab1c2b5bf3e1f7ce4875
Gong Hyeonsik <hyeonsik.gong@gmail.com>
2019-03-09 04:10:14 +0900

    update plan

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,15 @@
-<template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+<template lang="pug">
+  .v-app#app
+    MainPage
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import MainPage from './page/MainPage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainPage
   },
   data () {
     return {
@@ -36,3 +18,14 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="sass">
+
+*
+  padding: 0
+  margin: 0
+  -webkit-box-sizing: border-box
+  -moz-box-sizing: border-box
+  box-sizing: border-box
+
+</style>
---
+++ src/page/MainPage.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .wrapper
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+};
+</script>
+
+<style scoped lang="sass">
+
+.wrapper
+  width: 1920px
+  height: 1080px
+  background: silver
+
+
+</style>
